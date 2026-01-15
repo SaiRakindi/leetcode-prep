70d97942defcb5ddd006f6e6f225e34829fbebe9
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-15 23:28:09 +0530

    feat: optimized approach 1

--- array/268/missing-number-in-array.js
+++ array/268/missing-number-in-array.js
@@ -8,17 +8,30 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
-  for (let i = 1; i <= nums.length; i++) {
-    let isFound = false;
-    for (let j = 0; j <= nums.length; j++) {
-      if (nums[j] === i) {
-        isFound = true;
-        break;
-      }
-    }
+  //brute force
+  // for (let i = 1; i <= nums.length; i++) {
+  //     let isFound = false;
+  //     for(let j = 0; j <= nums.length; j++) {
+  //         if (nums[j] === i) {
+  //             isFound = true;
+  //             break;
+  //         }
+  //     }
 
-    if (!isFound) return i;
-  }
+  //     if (!isFound) return i;
 
-  return 0;
+  // }
+
+  // return 0;
+
+  //Optimzed approach
+  let arrayLength = nums.length;
+  const nNaturalNumbersSum = (arrayLength * (arrayLength + 1)) / 2;
+
+  const sumOfNumbers = nums.reduce((acc, currentValue) => {
+    acc += currentValue;
+    return acc;
+  }, 0);
+
+  return nNaturalNumbersSum - sumOfNumbers;
 };
