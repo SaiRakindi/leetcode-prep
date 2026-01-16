889ed33228b30a2ce4f1fbead96a9230eb16d6b9
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-16 23:48:13 +0530

    feat: single number better approach

--- array/136/single-number.js
+++ array/136/single-number.js
@@ -4,18 +4,37 @@
  */
 var singleNumber = function (nums) {
   //Brute Force
+  // let numsLength = nums.length;
+
+  // for (let i = 0; i < numsLength; i++) {
+  //     let num = nums[i]
+  //     let count = 0;
+
+  //     for (let j = 0; j < numsLength; j++) {
+  //         if (nums[j] === num) {
+  //             count++;
+  //         }
+  //     }
+
+  //     if (count === 1) return num
+
+  // }
+
+  //Better Approach
   let numsLength = nums.length;
+  let maxElement = Math.max(...nums);
+
+  let hashArray = Array(maxElement + 1).fill(0);
 
   for (let i = 0; i < numsLength; i++) {
-    let num = nums[i];
-    let count = 0;
-
-    for (let j = 0; j < numsLength; j++) {
-      if (nums[j] === num) {
-        count++;
-      }
-    }
-
-    if (count === 1) return num;
+    hashArray[nums[i]]++;
   }
+
+  for (let i = 0; i < numsLength; i++) {
+    if (hashArray[nums[i]] === 1) {
+      return nums[i];
+    }
+  }
+
+  return -1;
 };
