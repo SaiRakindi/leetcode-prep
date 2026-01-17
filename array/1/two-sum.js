aa3cd76db2441d25943c04bb6545e6a5b6cf5e44
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-17 22:54:53 +0530

    feat: two sum

---
+++ array/1/two-sum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  //using js array methods
+  // for (let i = 0; i < nums.length; i++) {
+  //     let requiredNumber = target - nums[i]
+  //     let requiredNumberIndex = nums.indexOf(requiredNumber)
+
+  //     if (i !== requiredNumberIndex && requiredNumberIndex !== -1) return [i, requiredNumberIndex]
+
+  // }
+
+  //Brute force
+  let numsLength = nums.length;
+  for (let i = 0; i < numsLength; i++) {
+    for (let j = 0; j < numsLength; j++) {
+      if (i !== j) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+      }
+    }
+  }
+};
