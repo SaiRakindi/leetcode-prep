4d941c943732e211b21856324dc7011982e99e20
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-18 22:53:04 +0530

    feat: binary search recursive approach

--- array/704/binary-search.js
+++ array/704/binary-search.js
@@ -4,16 +4,33 @@
  * @return {number}
  */
 var search = function (nums, target) {
+  //Iterative appraoch
+  // let low = 0, high = nums.length - 1;
+
+  // while (low <= high) {
+  //     let mid = Math.floor((low + high) / 2)
+
+  //     if (nums[mid] === target) return mid;
+  //     else if (nums[mid] < target) low = mid + 1;
+  //     else high = mid - 1;
+  // }
+
+  // return -1;
+
+  //Recursive approach
+
   let low = 0,
     high = nums.length - 1;
+  function recursiveSearch(arr, low, high, target) {
+    if (high < low) return -1;
 
-  while (low <= high) {
     let mid = Math.floor((low + high) / 2);
 
-    if (nums[mid] === target) return mid;
-    else if (nums[mid] < target) low = mid + 1;
-    else high = mid - 1;
+    if (arr[mid] === target) return mid;
+    else if (target > arr[mid])
+      return recursiveSearch(arr, mid + 1, high, target);
+    return recursiveSearch(arr, low, mid - 1, target);
   }
 
-  return -1;
+  return recursiveSearch(nums, low, high, target);
 };
