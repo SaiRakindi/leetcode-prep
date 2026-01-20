7740da4ddace810287ed804c69e1e786e6af062e
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-20 23:47:53 +0530

    feat: first last occurences using binary search

--- binary-search/34/first-last-occurence.js
+++ binary-search/34/first-last-occurence.js
@@ -4,26 +4,60 @@
  * @return {number[]}
  */
 var searchRange = function (nums, target) {
-  let firstIndex = -1,
-    lastIndex = -1;
+  //Brute force
+  // let firstIndex = -1, lastIndex = -1;
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === target) {
-      for (let i = 0; i < nums.length; i++) {
-        //   if (firstIndex === -1) {
-        //     firstIndex = i;
-        //     lastIndex = i;
-        //   } else if (firstIndex !== -1 && lastIndex !== -1) {
-        //     lastIndex = i;
-        //   }
+  // for (let i = 0; i < nums.length; i++) {
+  //     if (nums[i] === target) {
+  //         if (firstIndex === -1 )  firstIndex = i;
+  //         lastIndex = i;
 
-        if (nums[i] === target) {
-          if (firstIndex === -1) firstIndex = i;
-          lastIndex = i;
-        }
-      }
+  //     }
+  // }
+
+  // return [firstIndex, lastIndex];
+
+  //Binary search approach
+  let numsLength = nums.length;
+
+  function firstPosition(nums, target) {
+    let low = 0,
+      high = numsLength - 1;
+    let firstIndex = -1;
+
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+
+      if (nums[mid] === target) {
+        firstIndex = mid;
+        high = mid - 1;
+      } else if (nums[mid] > target) high = mid - 1;
+      else low = mid + 1;
     }
+
+    return firstIndex;
   }
 
-  return [firstIndex, lastIndex];
+  function lastPosition(nums, target) {
+    let low = 0,
+      high = numsLength - 1;
+    let lastIndex = -1;
+
+    while (low <= high) {
+      let mid = Math.floor((low + high) / 2);
+
+      if (nums[mid] === target) {
+        lastIndex = mid;
+        low = mid + 1;
+      } else if (nums[mid] < target) low = mid + 1;
+      else high = mid - 1;
+    }
+
+    return lastIndex;
+  }
+
+  let firstOccurence = firstPosition(nums, target);
+  let lastOccurence = lastPosition(nums, target);
+
+  return [firstOccurence, lastOccurence];
 };
