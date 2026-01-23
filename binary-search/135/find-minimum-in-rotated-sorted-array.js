2bb29ebbb63b9a93a715c392889814a3a146bada
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-23 23:40:25 +0530

    feat: optimal appraoch using binary search for minimum in a rotated sorted array

--- binary-search/135/find-minimum-in-rotated-sorted-array.js
+++ binary-search/135/find-minimum-in-rotated-sorted-array.js
@@ -4,11 +4,43 @@
  */
 var findMin = function (nums) {
   //Brute force approach
-  let min = nums[0];
+  // let min = nums[0];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < min) min = nums[i];
+  // for (let i = 0; i < nums.length; i++) {
+  //     if (nums[i] < min) min = nums[i];
+  // }
+
+  // return min;
+
+  // Optimal approach using binary search
+  let low = 0,
+    high = nums.length - 1,
+    minimum = Number.POSITIVE_INFINITY;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[low] <= nums[mid]) {
+      minimum = Math.min(minimum, nums[low]);
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+      minimum = Math.min(minimum, nums[mid]);
+    }
   }
 
-  return min;
+  return minimum;
+
+  //Optimal approach using binary search without minimum variable
+  // let low = 0, high = nums.length - 1
+
+  // while (low < high) {
+  //     let mid = Math.floor((low + high) / 2);
+
+  //     if (nums[mid] > nums[high]) low = mid + 1;
+  //     else high = mid;
+
+  // }
+
+  // return nums[low];
 };
