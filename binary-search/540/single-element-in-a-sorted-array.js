27db77914e249353ae85381f613f2868a745bfac
SaiRakindi <sai.rakindi3@gmail.com>
2026-01-24 23:41:13 +0530

    feat: optimal approach to find single element in a sorted array

--- binary-search/540/single-element-in-a-sorted-array.js
+++ binary-search/540/single-element-in-a-sorted-array.js
@@ -4,16 +4,50 @@
  */
 var singleNonDuplicate = function (nums) {
   //Brute force approach
+  // let numsLength = nums.length;
+  // for (let i = 0; i < numsLength; i++) {
+  //     //We check if the previous element or the next element to be same.
+  //     //If the elements are not same, that is non duplicate element.
+  //     if (i === 0) {
+  //         if (nums[i] !== nums [i + 1]) return nums[i];
+  //     }
+
+  //     else if (i === numsLength - 1) {
+  //         if (nums[i - 1] !== nums[i]) return nums[i];
+  //     }
+  //     else {
+  //         if (nums[i - 1] !== nums[i] && nums[i] !== nums[i + 1]) return nums[i];
+  //     }
+  // }
+
+  //Optimized approach using binary search;
   let numsLength = nums.length;
-  for (let i = 0; i < numsLength; i++) {
-    //We check if the previous element or the next element to be same.
-    //If the elements are not same, that is non duplicate element.
-    if (i === 0) {
-      if (nums[i] !== nums[i + 1]) return nums[i];
-    } else if (i === numsLength - 1) {
-      if (nums[i - 1] !== nums[i]) return nums[i];
-    } else {
-      if (nums[i - 1] !== nums[i] && nums[i] !== nums[i + 1]) return nums[i];
+
+  if (numsLength === 1) return nums[0];
+
+  if (nums[0] !== nums[1]) return nums[0];
+
+  if (nums[numsLength - 1] !== nums[numsLength - 2])
+    return nums[numsLength - 1];
+
+  let low = 1;
+  high = numsLength - 2;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1])
+      return nums[mid];
+
+    //We are on the left half
+    if (
+      (mid % 2 === 1 && nums[mid] === nums[mid - 1]) ||
+      (mid % 2 === 0 && nums[mid] === nums[mid + 1])
+    ) {
+      low = mid + 1;
     }
+
+    //We are on the right half
+    else high = mid - 1;
   }
 };
