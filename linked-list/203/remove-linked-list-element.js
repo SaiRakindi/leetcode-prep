e49bfbe7925330a025784450f28564ebc1df59a6
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-08 23:25:57 +0530

    feat: linked list code with explanation

--- linked-list/203/remove-linked-list-element.js
+++ linked-list/203/remove-linked-list-element.js
@@ -11,13 +11,17 @@
  * @return {ListNode}
  */
 var removeElements = function (head, val) {
-  if (!head) return head;
+  if (!head) return head; //return head if there is no head
 
   while (head) {
+    //if we have head and if the head has the removabale node value, we remove the head and make the next node as head.
     if (head.val === val) head = head.next;
     else break;
   }
 
+  //We check whether the current node has value and it is not the tail node and it has the value with the desired value
+  //We remove it and point the next node as the next to next node.
+  //If not found, we move to the next node.
   let current = head;
 
   while (current && current.next) {
