10c36fd7700bce2d76c4eeeebf02e34dadc7e867
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-10 22:03:17 +0530

    feat: detect cycle optimization

--- linked-list/142/linked-list-II.js
+++ linked-list/142/linked-list-II.js
@@ -11,17 +11,38 @@
  * @return {ListNode}
  */
 var detectCycle = function (head) {
-  //Brute force approach
-  let nodeSet = new Set();
+  //Using hash -> set in js
+  // let nodeSet = new Set();
 
-  while (head) {
-    if (nodeSet.has(head)) return head;
+  // while (head) {
+  //     if (nodeSet.has(head)) return head;
 
-    nodeSet.add(head);
+  //     nodeSet.add(head);
 
-    head = head.next;
+  //     head = head.next;
+  // }
+
+  // return null;
+
+  //Tortoise Hare method
+  let slow = head,
+    fast = head;
+
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+
+    if (slow == fast) {
+      slow = head;
+
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+
+      return slow;
+    }
   }
 
-  //Return null if there is no cycle node
   return null;
 };
