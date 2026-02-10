81e0f5bba99053d6a364f117ad2074026542402d
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-10 23:33:28 +0530

    feat: delete middle node using tortoise hare method

--- linked-list/2095/delete-middle-node-of-linked-list.js
+++ linked-list/2095/delete-middle-node-of-linked-list.js
@@ -10,31 +10,51 @@
  * @return {ListNode}
  */
 var deleteMiddle = function (head) {
-  let listLength = 0,
-    temp = head;
+  //Brute force - TC: O(N+N/2), SC:O(1)
+  // let listLength = 0, temp = head;
+
+  // if (!head) return head;
+
+  // if (!head.next) return head.next
+
+  // while (temp) {
+  //     listLength++;
+  //     temp = temp.next;
+  // }
+
+  // let middleIndex = Math.floor(listLength / 2), startNode = head;
+
+  // while (startNode) {
+  //     middleIndex--;
+
+  //     if (middleIndex === 0) {
+  //         let middleNode = startNode.next;
+  //         startNode.next = startNode.next.next;
+  //     }
+
+  //     startNode = startNode.next;
+  // }
+
+  // return head;
+
+  //Optimal appraoch;
+  let slow = head,
+    fast = head;
 
   if (!head) return head;
 
   if (!head.next) return head.next;
 
-  while (temp) {
-    listLength++;
-    temp = temp.next;
+  //To point slow to the prev of middle node, we skip the initial slow next node
+  fast = fast.next.next;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
   }
 
-  let middleIndex = Math.floor(listLength / 2),
-    startNode = head;
-
-  while (startNode) {
-    middleIndex--;
-
-    if (middleIndex === 0) {
-      let middleNode = startNode.next;
-      startNode.next = startNode.next.next;
-    }
-
-    startNode = startNode.next;
-  }
+  let newNode = slow.next;
+  slow.next = slow.next.next;
 
   return head;
 };
