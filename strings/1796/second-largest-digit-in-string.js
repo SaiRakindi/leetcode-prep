f2a6ce8ebfe6a316dfd7b8e31914114ef51143ca
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-12 23:59:32 +0530

    feat: optimal appraoch using js

--- strings/1796/second-largest-digit-in-string.js
+++ strings/1796/second-largest-digit-in-string.js
@@ -3,19 +3,32 @@
  * @return {number}
  */
 var secondHighest = function (s) {
-  let numsArray = [];
+  //Brute force
+  // let numsArray = [];
 
-  [...s].forEach((char) => {
-    if (
-      !isNaN(char) &&
-      typeof Number(char) === "number" &&
-      numsArray.lastIndexOf(Number(char)) === -1
-    ) {
-      numsArray.push(Number(char));
-    }
-  });
+  // [...s].forEach((char) => {
+  //     if (
+  //         !isNaN(char) &&
+  //         typeof Number(char) === "number" &&
+  //         numsArray.lastIndexOf(Number(char)) === -1
+  //         ) {
+  //         numsArray.push(Number(char));
+  //     };
+  // });
 
-  numsArray.sort((a, b) => b - a);
+  // numsArray.sort((a, b) => b - a)
 
-  return numsArray.length >= 2 ? numsArray[1] : -1;
+  // return numsArray.length >= 2 ? numsArray[1] : -1;
+
+  //Optimal approach using JS features
+  const digitsSet = new Set();
+
+  for (const char of s) {
+    if (s >= "0" && char <= "9") digitsSet.add(Number(char));
+  }
+
+  const digits = Array.from(digitsSet);
+  digits.sort((a, b) => b - a);
+
+  return digits.length >= 2 ? digits[1] : -1;
 };
