da44a15e9c086a9defbf9b4486b845b18d43f228
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-19 23:36:43 +0530

    feat: valid parenthese 2 approaches

--- stacks/20-valid-parentheses.js
+++ stacks/20-valid-parentheses.js
@@ -3,24 +3,38 @@
  * @return {boolean}
  */
 var isValid = function (s) {
+  //Approach 1
+  // let parenthesesArray = [];
+
+  // for (let char of s) {
+  //     if (char === "(" || char === "[" || char === "{") parenthesesArray.push(char);
+  //     else {
+  //         if (parenthesesArray.length === 0) return false;
+  //         else {
+  //             let lastParentheses = parenthesesArray.pop();
+
+  //             if ((char === ")" && lastParentheses === "(") ||
+  //                 (char === "]" && lastParentheses === "[") ||
+  //                 (char === "}" && lastParentheses === "{")) {
+  //                 continue;
+  //             }
+  //             else return false;
+  //         }
+  //     }
+  // }
+
+  // return parenthesesArray.length === 0;
+
+  //Approach 2
   let parenthesesArray = [];
 
   for (let char of s) {
-    if (char === "(" || char === "[" || char === "{")
-      parenthesesArray.push(char);
+    if (char === "(") parenthesesArray.push(")");
+    else if (char === "{") parenthesesArray.push("}");
+    else if (char === "[") parenthesesArray.push("]");
     else {
-      if (parenthesesArray.length === 0) return false;
-      else {
-        let lastParentheses = parenthesesArray.pop();
-
-        if (
-          (char === ")" && lastParentheses === "(") ||
-          (char === "]" && lastParentheses === "[") ||
-          (char === "}" && lastParentheses === "{")
-        ) {
-          continue;
-        } else return false;
-      }
+      let lastBracket = parenthesesArray.pop();
+      if (lastBracket !== char) return false;
     }
   }
 
