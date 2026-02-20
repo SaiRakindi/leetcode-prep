ec7c508e5ad3bfd68adbd588b5dbd0c1c0e6cfba
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-20 22:27:50 +0530

    feat: optimized solution

--- stacks-queues/155-min-stack.js
+++ stacks-queues/155-min-stack.js
@@ -1,3 +1,4 @@
+//My solution
 var MinStack = function () {
   this.stack = [];
 };
@@ -39,6 +40,56 @@ MinStack.prototype.getMin = function () {
 
     return minElement;
   }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+//Approach 2
+
+var MinStack = function () {
+  this.stack = [];
+};
+
+/**
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function (val) {
+  let minValue = this.getMin();
+
+  if (minValue === null || minValue > val) {
+    minValue = val;
+  }
+
+  this.stack.push([val, minValue]);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  return this.stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this.stack[this.stack.length - 1][0];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+  return this.stack.length ? this.stack[this.stack.length - 1][1] : null;
 };
 
 /**
