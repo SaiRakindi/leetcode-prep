7be23e1da8d4231573e74a2b6bf9b122ee0dfa42
SaiRakindi <sai.rakindi3@gmail.com>
2026-02-21 23:10:11 +0530

    feat: greater element optimal solution

--- stacks-queues/503-greater-element-II.js
+++ stacks-queues/503-greater-element-II.js
@@ -3,20 +3,45 @@
  * @return {number[]}
  */
 var nextGreaterElements = function (nums) {
+  //Brute force approach
+  // let numsLength = nums.length;
+
+  // let nextGreaterElements = new Array(numsLength).fill(-1);
+
+  // for (let i = 0; i < numsLength; i++) {
+  //     let currentElement = nums[i]
+  //     for (let j = 1; j < numsLength; j++) {
+  //         let index = (j + i) % numsLength;
+
+  //         if (nums[index] > currentElement) {
+  //             nextGreaterElements[i] = nums[index];
+  //             break;
+  //         }
+  //     }
+  // }
+
+  // return nextGreaterElements;
+
+  //Optimal approach
   let numsLength = nums.length;
 
   let nextGreaterElements = new Array(numsLength).fill(-1);
 
-  for (let i = 0; i < numsLength; i++) {
-    let currentElement = nums[i];
-    for (let j = 1; j < numsLength; j++) {
-      let index = (j + i) % numsLength;
+  let stack = [];
 
-      if (nums[index] > currentElement) {
-        nextGreaterElements[i] = nums[index];
-        break;
-      }
+  for (let i = 2 * numsLength - 1; i >= 0; i--) {
+    let currentIndex = i % numsLength;
+
+    let currentElement = nums[currentIndex];
+
+    while (stack.length && stack[stack.length - 1] <= currentElement) {
+      stack.pop();
     }
+
+    if (i < numsLength)
+      nextGreaterElements[i] = stack.length > 0 ? stack[stack.length - 1] : -1;
+
+    stack.push(currentElement);
   }
 
   return nextGreaterElements;
