e7361370480d7adc454ac06fa0fc1e265937f2e8
SaiRakindi <sai.rakindi3@gmail.com>
2026-03-11 23:26:05 +0530

    feat: valid parentesis checker dp approach

--- greedy-algorithms/678-valid-parenthesis-string.js
+++ greedy-algorithms/678-valid-parenthesis-string.js
@@ -3,24 +3,43 @@
  * @return {boolean}
  */
 var checkValidString = function (s) {
-  //Using recursion
-  function isValid(str, index, count) {
-    if (count < 0) return false;
+  //Brute force approach - Using recursion
+  // function isValid(str, index, count) {
+  //     if (count < 0) return false;
 
-    if (index === s.length) return count === 0;
+  //     if (index === s.length) return count === 0;
 
-    const currentChar = s[index];
+  //     const currentChar = s[index];
 
-    if (currentChar === "(") return isValid(str, index + 1, count + 1);
-    else if (currentChar === ")") return isValid(str, index + 1, count - 1);
-    else {
-      return (
-        isValid(str, index + 1, count + 1) ||
-        isValid(str, index + 1, count - 1) ||
-        isValid(str, index + 1, count)
-      );
+  //     if (currentChar === "(") return isValid(str, index + 1, count + 1)
+  //     else if (currentChar === ")") return isValid(str, index + 1, count - 1);
+  //     else {
+  //         return isValid(str, index + 1, count + 1) || isValid(str, index + 1, count - 1) || isValid(str, index + 1, count)
+  //     }
+  // }
+
+  // return isValid(s, 0, 0)
+
+  //Optimal solution
+  let minimum = 0,
+    maximum = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      minimum += 1;
+      maximum += 1;
+    } else if (s[i] === ")") {
+      minimum -= 1;
+      maximum -= 1;
+    } else {
+      minimum -= 1;
+      maximum += 1;
     }
+
+    if (minimum < 0) minimum = 0;
+
+    if (maximum < 0) return false;
   }
 
-  return isValid(s, 0, 0);
+  return minimum === 0;
 };
