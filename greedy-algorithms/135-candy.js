082ad74e26b3bd77d8ffb7c03a04be7007377bb9
SaiRakindi <sai.rakindi3@gmail.com>
2026-03-13 23:37:38 +0530

    feat: candy with slightly better approach

--- greedy-algorithms/135-candy.js
+++ greedy-algorithms/135-candy.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 var candy = function (ratings) {
-  //Bruteforce approach
+  // Bruteforce approach - TC: O(3N), SC: O(2N)
 
   let ratingsLength = ratings.length;
 
+  // let left = Array(ratingsLength).fill(1);
+  // let right = Array(ratingsLength).fill(1);
   let left = [],
     right = [];
 
@@ -28,4 +30,32 @@ var candy = function (ratings) {
   }
 
   return sum;
+
+  // //Slightly better approach
+  // let ratingsLength = ratings.length;
+
+  // let left = Array(ratingsLength).fill(1);
+  // // let right = Array(ratingsLength).fill(1);
+
+  // left[0] = 1;
+  // //  right[ratingsLength - 1] = 1;
+
+  // for (let i = 1; i < ratingsLength; i++) {
+  //     if (ratings[i] > ratings[i - 1]) left[i] = left[i - 1] + 1;
+  //     else left[i] = 1;
+  // }
+
+  // let current = 1, right = 1, sum = Math.max(1, left[ratingsLength - 1]);
+
+  // for (let i = ratingsLength - 2; i >= 0; i--) {
+  //     if (ratings[i] > ratings[i + 1]) {
+  //         current = right + 1;
+  //         right = current;
+  //     }
+  //     else current = 1;
+
+  //     sum += Math.max(left[i], current);
+  // }
+
+  // return sum;
 };
