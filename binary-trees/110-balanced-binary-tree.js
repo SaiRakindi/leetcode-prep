4fb1088b26607b1fa3ce022880694a80cc5ed21a
SaiRakindi <sai.rakindi3@gmail.com>
2026-03-24 23:42:53 +0530

    feat: balanced binary tree

--- binary-trees/110-balanced-binary-tree.js
+++ binary-trees/110-balanced-binary-tree.js
@@ -12,44 +12,42 @@
  */
 var isBalanced = function (root) {
   //Brute force approach
+  //   function getHeight(root) {
+  //     if (!root) return 0;
 
+  //     let leftHeight = getHeight(root.left);
+  //     let rightHeight = getHeight(root.right);
+
+  //     return 1 + Math.max(leftHeight, rightHeight);
+  //   }
+
+  //   if (!root) return true;
+
+  //   let leftHeight = getHeight(root.left);
+  //   let rightHeight = getHeight(root.right);
+
+  //   if (
+  //     Math.abs(leftHeight - rightHeight) <= 1 &&
+  //     isBalanced(root.left) &&
+  //     isBalanced(root.right)
+  //   )
+  //     return true;
+
+  //   return false;
+
+  //     //Optimal approach;
   function getHeight(root) {
     if (!root) return 0;
 
     let leftHeight = getHeight(root.left);
     let rightHeight = getHeight(root.right);
 
+    if (leftHeight === -1 || rightHeight === -1) return -1;
+
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
     return 1 + Math.max(leftHeight, rightHeight);
   }
 
-  if (!root) return true;
-
-  let leftHeight = getHeight(root.left);
-  let rightHeight = getHeight(root.right);
-
-  if (
-    Math.abs(leftHeight - rightHeight) <= 1 &&
-    isBalanced(root.left) &&
-    isBalanced(root.right)
-  )
-    return true;
-
-  return false;
-
-  // function getHeight (root) {
-  //     //Optimal approach;
-
-  //     if (!root) return 0;
-
-  //     let leftHeight = getHeight(root.left);
-  //     let rightHeight = getHeight(root.right);
-
-  //     if (leftHeight === - 1 || rightHeight === -1) return -1;
-
-  //     if (Math.abs(leftHeight - rightHeight) > 1) return -1;
-
-  //     return 1 + Math.max(leftHeight, rightHeight)
-  // }
-
-  // return getHeight(root) !== -1;
+  return getHeight(root) !== -1;
 };
