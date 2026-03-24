fb6a2019305afaa176703599e334c025b34c2ab8
SaiRakindi <sai.rakindi3@gmail.com>
2026-03-24 23:40:50 +0530

    feat: brute force approach

--- binary-trees/110-balanced-binary-tree.js
+++ binary-trees/110-balanced-binary-tree.js
@@ -11,20 +11,45 @@
  * @return {boolean}
  */
 var isBalanced = function (root) {
-  function getHeight(root) {
-    //Optimal approach;
+  //Brute force approach
 
+  function getHeight(root) {
     if (!root) return 0;
 
     let leftHeight = getHeight(root.left);
     let rightHeight = getHeight(root.right);
 
-    if (leftHeight === -1 || rightHeight === -1) return -1;
-
-    if (Math.abs(leftHeight - rightHeight) > 1) return -1;
-
     return 1 + Math.max(leftHeight, rightHeight);
   }
 
-  return getHeight(root) !== -1;
+  if (!root) return true;
+
+  let leftHeight = getHeight(root.left);
+  let rightHeight = getHeight(root.right);
+
+  if (
+    Math.abs(leftHeight - rightHeight) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  )
+    return true;
+
+  return false;
+
+  // function getHeight (root) {
+  //     //Optimal approach;
+
+  //     if (!root) return 0;
+
+  //     let leftHeight = getHeight(root.left);
+  //     let rightHeight = getHeight(root.right);
+
+  //     if (leftHeight === - 1 || rightHeight === -1) return -1;
+
+  //     if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+
+  //     return 1 + Math.max(leftHeight, rightHeight)
+  // }
+
+  // return getHeight(root) !== -1;
 };
